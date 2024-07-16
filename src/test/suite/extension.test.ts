1d84d2228346205ed01c84169d80ec70b2e76376
Ramona Hartinger <r.hartinger@adito.de>
2024-07-16 14:42:02 +0200

    test: added a test for activating the extension

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,25 @@
 import { DialogValues } from "@aditosoftware/vscode-input";
-import { createGeneralStatusBarItem, setRequireForceAndForceParameter, validateInput } from "../../extension";
+import { activate, createGeneralStatusBarItem, setRequireForceAndForceParameter, validateInput } from "../../extension";
 import assert from "assert";
 import { expect } from "chai";
 import { Command } from "vscode";
 import * as vscode from "vscode";
+import { TestUtils } from "./TestUtils";
+import Sinon from "sinon";
+import { randomUUID } from "crypto";
 
 /**
  * Tests the `extension.ts` file.
  * If you want to test the command more integration test like, then use `extension.it.test.ts`.
  */
 suite("extension tests", () => {
+  /**
+   * Restores all stubs after the commands.
+   */
+  teardown("restore stubs", () => {
+    Sinon.restore();
+  });
+
   /**
    * Tests the validation of the inputs.
    */
@@ -93,6 +103,53 @@ suite("extension tests", () => {
       // check that the command is a valid command
       const allCommands = await vscode.commands.getCommands();
       expect(allCommands).to.include(command.command);
+    });
+  });
+
+  /**
+   * Tests the `activate` method.
+   */
+  suite("activate", () => {
+    /**
+     * Tests that the activation of the extension does work.
+     */
+    test("should activate", async () => {
+      const tempDir = TestUtils.createTempFolderForTests("extension");
+
+      const memento: vscode.Memento = {
+        keys: () => {
+          throw new Error("not needed for tests");
+        },
+        get: () => {
+          return undefined;
+        },
+        update: async () => {},
+      };
+
+      const names: string[] = [];
+
+      // don't register the commands real, because
+      const registerCommand = Sinon.stub(vscode.commands, "registerCommand");
+      registerCommand.callsFake((command, callback, thisArgs) => {
+        const newCommandName = `${randomUUID()}-${command}`;
+        names.push(newCommandName);
+
+        return registerCommand.wrappedMethod.apply(vscode.commands, [newCommandName, callback, thisArgs]);
+      });
+
+      const context: vscode.ExtensionContext = {
+        globalStorageUri: vscode.Uri.file(tempDir),
+        extensionPath: tempDir,
+        extensionUri: vscode.Uri.file(tempDir),
+        logUri: vscode.Uri.file(tempDir),
+        subscriptions: [],
+        globalState: memento,
+      } as unknown as vscode.ExtensionContext;
+
+      await activate(context);
+
+      // check that there were any subscriptions registered
+      expect(context.subscriptions).to.have.lengthOf.above(20);
     });
   });
 });
